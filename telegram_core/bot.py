74498a34653776fef053f44c86950bce2f76280c
Коля <kopytovnn@gmailcom>
2024-07-03 14:26:01 +0300

    Add limitation for users

--- telegram_core/bot.py
+++ telegram_core/bot.py
@@ -10,6 +10,10 @@ from app.handlers import common, bybit_handler, bingx_handler
 
 from aiogram.filters import CommandStart
 
+from aiogram import BaseMiddleware
+from typing import Callable, Dict, Any, Awaitable
+from aiogram.types import TelegramObject
+
 from aiogram.types import Message
 
 import multiprocessing as mp
@@ -18,6 +22,27 @@ from fix.Bybit.main import start
 
 
 # from telegram_core.app.handlers import bybit_handler
+
+class SomeMiddleware(BaseMiddleware):
+    def __init__(self, allowed_users):
+        super().__init__()
+        self.allowed_users = allowed_users
+
+    async def __call__(
+        self,
+        handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
+        event: TelegramObject,
+        data: Dict[str, Any]
+    ) -> Any:
+        # print(event.chat)
+        user_id = event.chat.id
+        if user_id not in self.allowed_users:
+            return 0
+            # raise CancelHandler()  # Остановка дальнейшей обработки
+        result = await handler(event, data)
+        print("After handler")
+        return result
+
 
 
 async def main():
@@ -28,6 +53,16 @@ async def main():
 
     dp = Dispatcher(storage=MemoryStorage())
     bot = Bot(TOKEN)
+
+    allowed_users = [540862463,
+                     6111704436,
+                     7493850317,
+                     6755106282,
+                     925216062]
+    common.router.message.middleware(SomeMiddleware(allowed_users))
+    bybit_handler.router.message.middleware(SomeMiddleware(allowed_users))
+    bingx_handler.router.message.middleware(SomeMiddleware(allowed_users))
+
 
     dp.include_router(common.router)
     dp.include_router(bybit_handler.router)
